0ec3a776139b80df5f34e3004bcd43027a99352f
Marco Fruh <marco.fruh@me.com>
2016-02-19 14:50:09 +0100

    Version 1.03

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var app = express();
 var cx = '008709307696191659990:81dznmj-lmg';
 var api = process.env.API;
-console.log('FOR PRODUCTION: API: ' + api);
 var history = [];
 
 app.use(cookieParser());
@@ -31,21 +30,24 @@ app.get('/*', function(req, res) {
 		when: new Date
 	});
 
+	console.log('SEARCH: ', path);
 	client.get(path, function(err, response, body) {
 		if (err || response.statusCode !== 200) {
 			console.log('Error: ' + err, response.statusCode);
 			return response.sendStatus(500);
 		}
 
-		for (var i = 0; i < body.items.length; i++) {
-			arr.push({
-				image: (body.items[i].pagemap.cse_image ? body.items[i].pagemap.cse_image[0].src : 'n/a'),
-				thumbnail: (body.items[i].pagemap.cse_thumbnail ? body.items[i].pagemap.cse_thumbnail[0].src : 'n/a'),
-				snippet: body.items[i].snippet,
-				context: body.items[i].link});
+		if (body.items) {
+			for (var i = 0; i < body.items.length; i++) {
+				arr.push({
+					image: (body.items[i].pagemap.cse_image ? body.items[i].pagemap.cse_image[0].src : 'n/a'),
+					thumbnail: (body.items[i].pagemap.cse_thumbnail ? body.items[i].pagemap.cse_thumbnail[0].src : 'n/a'),
+					snippet: body.items[i].snippet,
+					context: body.items[i].link});
+			}
+			res.json(arr);
 		}
-		res.json(arr);
-		});
+	});
 });
 
 app.set('port', process.env.PORT || 3000);
